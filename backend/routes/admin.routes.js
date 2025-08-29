69e8af4df58cc099640056bcb0d6e4a08572c690
Amulyaraj07 <amulyaraj1234@gmail.com>
2025-08-30 05:04:37 +0530

    added routes to manage events and registrations

--- backend/routes/admin.routes.js
+++ backend/routes/admin.routes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const adminController = require("../controllers/admin.controller");
+const eventController = require("../controllers/event.controller");
+const registrationController = require("../controllers/registration.controller");
 const { authAdmin } = require("../middlewares/auth.middleware");
 const { body } = require("express-validator");
 
@@ -28,5 +30,27 @@ router.post(
 router.get("/profile", authAdmin, adminController.getAdminProfile);
 
 router.post("/logout", authAdmin, adminController.logoutAdmin);
+
+router.post("/events", authAdmin, eventController.createEvent);
+
+router.put("/events/:id", authAdmin, eventController.updateEvent);
+
+router.delete("/events/:id", authAdmin, eventController.deleteEvent);
+
+router.get("/events", authAdmin, eventController.getAllEvents);
+
+router.get("/events/:id", authAdmin, eventController.getEventById);
+
+router.get(
+  "/events/:eventId/registrations",
+  authAdmin,
+  registrationController.getEventRegistrations
+);
+
+router.put(
+  "/registrations/:registrationId",
+  authAdmin,
+  registrationController.updateRegistrationStatus
+);
 
 module.exports = router;
